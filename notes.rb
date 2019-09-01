c29d2954fd55030cd7ab9ca08a2e11ac06ef7d85
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-01 23:11:56 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -17,7 +17,7 @@ def adding_matz
      }
    end
   
-   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}}
+   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
  
   programmer_hash
 
