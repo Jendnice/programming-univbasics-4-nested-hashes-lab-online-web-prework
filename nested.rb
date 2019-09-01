2cb7c884cd321bc2e09534173b45ec0ecc63e474
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-01 22:57:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,6 +70,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+        :yukihiro_matsumoto => {
+           :known_for => "Ruby",
+           :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -83,11 +87,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash.unshift[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]
-}
   programmer_hash
     
 end
+	
 
 def changing_alan
 	programmer_hash = 
